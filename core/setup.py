67c22ece970781e0ea6889034bf64f3471dc1a6a
Vijay Kiran <mail@vijaykiran.com>
2021-03-28 20:23:07 +0200

    WIP: The great Soda SQL package split
    - soda-sql-core install and soda command works (on my machine)
    - introduces soda-sql-postgresql and soda-sql-redshift packages

---
+++ core/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_namespace_packages
+
+if sys.version_info < (3, 7):
+    print('Error: Soda SQL requires at least Python 3.7')
+    print('Error: Please upgrade your Python version to 3.7 or later')
+    sys.exit(1)
+
+package_name = "soda-sql-core"
+package_version = "2.1.0b1"
+# TODO Add proper description
+description = "Soda SQL Core"
+
+requires = [
+    'click>=7.1.2',
+    'pyyaml>=5.4.1',
+    'Jinja2>=2.11.3',
+    'cryptography>=3.3.2'
+]
+# TODO Fix the params
+# TODO Add a warning that installing core doesn't give any warehouse functionality
+
+setup(
+    name=package_name,
+    version=package_version,
+    packages=find_namespace_packages(include=["sodasql*"]),
+    install_requires=requires,
+    entry_points={
+        "console_scripts":
+            [
+                "soda=sodasql.cli.cli:main"
+            ]
+    },
+)
---
+++ packages/redshift/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_namespace_packages
+
+if sys.version_info < (3, 7):
+    print('Error: Soda SQL requires at least Python 3.7')
+    print('Error: Please upgrade your Python version to 3.7 or later')
+    sys.exit(1)
+
+package_name = "soda-sql-redshift"
+package_version = "2.1.0b1"
+# TODO Add proper description
+description = "Soda SQL Redshift"
+
+requires = [
+    'soda-sql-core==2.1.0b1',
+    'soda-sql-postgresql==2.1.0b1'
+]
+# TODO Fix the params
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["sodasql*"])
+)
